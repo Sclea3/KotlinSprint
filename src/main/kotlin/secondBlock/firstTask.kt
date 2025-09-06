87888c1aa158f7d8fc8055e2c0ec5ef725a70f28
Sclea3 <sashamaty30@gmail.com>
2025-09-06 22:56:30 +0400

    KB 2-1

---
+++ src/main/kotlin/secondBlock/firstTask.kt
@@ -0,0 +1,10 @@
+package secondBlock
+
+fun main() {
+    val firstGrade = 3
+    val secondGrade = 4
+    val thirdGrade = 3
+    val fourthGrade = 5
+    val finalGrade: Double = (firstGrade + secondGrade + thirdGrade + fourthGrade) / 4.0
+    println(finalGrade)
+}
