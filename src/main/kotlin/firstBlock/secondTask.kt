00e96a4d6f2b95b6702bb36f2449353e0cb81432
Sclea3 <sashamaty30@gmail.com>
2025-09-07 10:22:28 +0400

    KB 2-1

--- src/main/kotlin/firstBlock/secondTask.kt
+++ src/main/kotlin/firstBlock/secondTask.kt
@@ -1,12 +1,12 @@
 package firstBlock
 
-    fun main() {
-        val ordersCount = 75
-        val greetingText = "Thanks for purchasing"
+fun main() {
+    val ordersCount = 75
+    val greetingText = "Thanks for purchasing"
 
-        print("$ordersCount\n$greetingText\n")
-        var workerCount = 2000
-//        print(workerCount)
-        workerCount = 1999
-        print(workerCount)
+    print("$ordersCount\n$greetingText\n")
+    var workerCount = 2000
+//    print(workerCount)
+    workerCount = 1999
+    print(workerCount)
     }
