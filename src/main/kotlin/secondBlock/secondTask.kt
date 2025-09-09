ecf2f0925e13224dbd75a62aed6effcb6ddd61ff
Sclea3 <sashamaty30@gmail.com>
2025-09-09 15:37:25 +0400

    KB 2-1 task done

--- src/main/kotlin/secondBlock/secondTask.kt
+++
@@ -1,9 +0,0 @@
-package secondBlock
-
-fun main() {
-    val studentScores = arrayOf(3, 4, 3, 5)
-    val sum = studentScores.sum()
-    val average = sum.toDouble() / studentScores.size
-
-    println("%.2f".format(average))
-}
