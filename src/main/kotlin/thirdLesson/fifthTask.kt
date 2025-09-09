3c623d0ad61465b82deeeadb022c2027d03cc21b
Sclea3 <sashamaty30@gmail.com>
2025-09-09 18:48:14 +0400

    KS 3-5 chess move split

--- src/main/kotlin/thirdLesson/fifthTask.kt
+++ src/main/kotlin/thirdLesson/fifthTask.kt
@@ -1,2 +1,12 @@
 package thirdLesson
 
+fun main() {
+    val inputString = "D2-D4;0"
+
+    val listFromString = inputString.split("-", ";")
+
+    val finalFrom: String = listFromString[0]
+    val finalTo: String = listFromString[1]
+    val finalMoveCtr: Int = listFromString[2].toInt()
+    println("$finalFrom $finalTo $finalMoveCtr")
+}
