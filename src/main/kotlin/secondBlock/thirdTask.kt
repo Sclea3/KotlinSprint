0626033135af7103b1d7013734483841e11e4059
Sclea3 <sashamaty30@gmail.com>
2025-09-10 00:27:02 +0400

    fixes

--- src/main/kotlin/secondBlock/thirdTask.kt
+++ src/main/kotlin/secondBlock/thirdTask.kt
@@ -1,14 +1,16 @@
 package secondBlock
 
+const val MINS_IN_HOUR = 60
+
 fun main() {
     val hoursDeparture = 9
     val minutesDeparture = 39
     val travelDurationMins = 457
 
-    val minutesFromDayStartDeparture = hoursDeparture * 60 + minutesDeparture
+    val minutesFromDayStartDeparture = hoursDeparture * MINS_IN_HOUR + minutesDeparture
     val minutesFromDayStartArrival = minutesFromDayStartDeparture + travelDurationMins
 
-    val hoursArrival = minutesFromDayStartArrival / 60
-    val minutesArrival = minutesFromDayStartArrival % 60
+    val hoursArrival = minutesFromDayStartArrival / MINS_IN_HOUR
+    val minutesArrival = minutesFromDayStartArrival % MINS_IN_HOUR
     print("Arrival time is $hoursArrival:$minutesArrival")
 }
