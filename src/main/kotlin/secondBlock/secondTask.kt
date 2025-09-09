a6b81398e8d620081c30387851d992350c6e10c4
Sclea3 <sashamaty30@gmail.com>
2025-09-09 16:00:25 +0400

    KS 2-2 second task done

---
+++ src/main/kotlin/secondBlock/secondTask.kt
@@ -0,0 +1,14 @@
+package secondBlock
+
+fun main() {
+    val staffCount = 50
+    val staffPersonalSalary = 30000
+    val internCount = 30
+    val internPersonalSalary = 20000
+
+    val staffSalary = staffCount * staffPersonalSalary
+    val commonSalary = staffSalary + (internPersonalSalary * internCount)
+
+    val averageSalaryPerEmployee = commonSalary / (internCount + staffCount)
+    print("Staff salary: $staffSalary\nCommon salary: $commonSalary\nAverage salary: $averageSalaryPerEmployee")
+}
