62a568eab5b97322388b4ba43efb517daea71955
Sclea3 <sashamaty30@gmail.com>
2025-09-09 18:05:10 +0400

    KS 3-2 idk how to name it

--- src/main/kotlin/thirdLesson/firstTask.kt
+++ src/main/kotlin/thirdLesson/firstTask.kt
@@ -1,2 +1,15 @@
 package thirdLesson
 
+fun main() {
+    val name = "Татьяна"
+    val secondName = "Сергеевна"
+    var surname = "Андреева"
+    var age = 20
+
+    println("$surname $name $secondName, $age")
+
+    surname = "Сидорова"
+    age = 22
+
+    println("$surname $name $secondName, $age")
+}
