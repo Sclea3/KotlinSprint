90eee9f8b5627e97f977fda11652e2e8833c532b
Sclea3 <sashamaty30@gmail.com>
2025-09-09 18:39:39 +0400

    KS 3-4 chess

--- src/main/kotlin/thirdLesson/fourthTask.kt
+++ src/main/kotlin/thirdLesson/fourthTask.kt
@@ -1,2 +1,13 @@
 package thirdLesson
 
+fun main() {
+    var from = "E2"
+    var to = "E4"
+    var moveCount = 1
+    println("$from:$to;$moveCount")
+
+    from = "D2"
+    to = "D3"
+    moveCount++
+    println("$from:$to;$moveCount")
+}
