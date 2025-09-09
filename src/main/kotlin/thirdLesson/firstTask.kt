6ad5e4c08a684c0358da4322b8f24e0bda6c7af5
Sclea3 <sashamaty30@gmail.com>
2025-09-09 18:01:25 +0400

    KS 3-1 greetings

--- src/main/kotlin/thirdLesson/firstTask.kt
+++ src/main/kotlin/thirdLesson/firstTask.kt
@@ -1,2 +1,9 @@
 package thirdLesson
 
+fun main() {
+    var greetingText = "Good day to you,"
+    val userName = "John"
+    println("$greetingText $userName")
+    greetingText = "Good evening,"
+    println("$greetingText $userName")
+}
