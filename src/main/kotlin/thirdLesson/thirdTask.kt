134a8552c7ff7a3e19654ccfeba98739337e8e74
Sclea3 <sashamaty30@gmail.com>
2025-09-10 10:36:40 +0400

    fixes

--- src/main/kotlin/thirdLesson/thirdTask.kt
+++ src/main/kotlin/thirdLesson/thirdTask.kt
@@ -2,12 +2,5 @@ package thirdLesson
 
 fun main() {
     val value = 3
-    multTable(value)
-}
-
-fun multTable(value: Int) {
-    val multiplicatorsList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
-    for (multiplicator in multiplicatorsList) {
-        println("$value x $multiplicator = ${value * multiplicator}")
-    }
+    println((1..9).joinToString("\n") { "$value x $it = ${value * it}" })
 }
