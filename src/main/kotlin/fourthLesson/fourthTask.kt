98d4c571dd1de946dda544651f040604f8f4190b
Sclea3 <sashamaty30@gmail.com>
2025-09-10 22:07:17 +0400

    fixes

--- src/main/kotlin/fourthLesson/fourthTask.kt
+++ src/main/kotlin/fourthLesson/fourthTask.kt
@@ -3,9 +3,6 @@ package fourthLesson
 fun main() {
     val dayOfTraining = 5
 
-    // Определяем группу мышц по четности дня
-    // День 1, 3, 5, 7... - руки и пресс
-    // День 2, 4, 6, 8... - ноги и спина
     val isArmsAndAbsDay = dayOfTraining % 2 == 1
 
     val message = """
