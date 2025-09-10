9f23511f7d7b26e8f238e6b699fabad11c443223
Sclea3 <sashamaty30@gmail.com>
2025-09-10 21:53:40 +0400

    KS-4-3 Weather for beans

--- src/main/kotlin/fourthLesson/thirdTask.kt
+++ src/main/kotlin/fourthLesson/thirdTask.kt
@@ -1,2 +1,27 @@
 package fourthLesson
 
+data class WeatherForBeans(
+    val isSunny: Boolean,
+    val isAwningOpen: Boolean,
+    val humidity: Int,
+    val season: String? = null,
+)
+
+fun main() {
+    val todayWeather = WeatherForBeans(
+        isSunny = true,
+        isAwningOpen = true,
+        humidity = 20,
+        season = "winter",
+    )
+    val perfectWeather = WeatherForBeans(
+        isSunny = true,
+        isAwningOpen = true,
+        humidity = 20,
+    )
+    println(chechIsWeatherSuitable(todayWeather, perfectWeather))
+}
+
+fun chechIsWeatherSuitable(todayWeather: WeatherForBeans, perfectWeather: WeatherForBeans): String {
+    return "Благоприятные ли условия сейчас для роста бобовых? ${todayWeather.isSunny == perfectWeather.isSunny && todayWeather.isAwningOpen == perfectWeather.isAwningOpen && todayWeather.humidity == perfectWeather.humidity && todayWeather.season != "winter"}"
+}
