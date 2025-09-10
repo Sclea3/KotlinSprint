2fae3ede513605a36bd1bb9283bd0a12582224af
Sclea3 <sashamaty30@gmail.com>
2025-09-10 22:05:37 +0400

    KS-4-4 Workout planning

--- src/main/kotlin/fourthLesson/fourthTask.kt
+++ src/main/kotlin/fourthLesson/fourthTask.kt
@@ -1,2 +1,19 @@
 package fourthLesson
 
+fun main() {
+    val dayOfTraining = 5
+
+    // Определяем группу мышц по четности дня
+    // День 1, 3, 5, 7... - руки и пресс
+    // День 2, 4, 6, 8... - ноги и спина
+    val isArmsAndAbsDay = dayOfTraining % 2 == 1
+
+    val message = """
+Упражнения для рук:       $isArmsAndAbsDay
+Упражнения для ног:       ${!isArmsAndAbsDay}
+Упражнения для спины:     ${!isArmsAndAbsDay}
+Упражнения для пресса:    $isArmsAndAbsDay
+    """.trimMargin()
+
+    println(message)
+}
