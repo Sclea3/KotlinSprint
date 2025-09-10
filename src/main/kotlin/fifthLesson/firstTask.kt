c864e8c13ab40065752fa59aa4cab6cab5ac36f4
Sclea3 <sashamaty30@gmail.com>
2025-09-10 22:49:19 +0400

    done after merge

---
+++ src/main/kotlin/fifthLesson/firstTask.kt
@@ -0,0 +1,14 @@
+package FifthLesson
+
+fun main() {
+    val firstInt = (1..99).random()
+    val secondInt = (1..99).random()
+    println("Введите сумму двух чисел: $firstInt + $secondInt")
+    val result = readLine()?.toIntOrNull()
+    if (result == firstInt + secondInt) {
+        println("Добро пожаловать!")
+    }
+    else {
+        println("Доступ запрещён.")
+    }
+}
