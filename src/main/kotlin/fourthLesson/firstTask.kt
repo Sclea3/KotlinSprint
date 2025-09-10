b695d2645afa614cf73321c6a29a5992d6cdb29b
Sclea3 <sashamaty30@gmail.com>
2025-09-10 15:30:48 +0400

    KS-4-1 restraunt tables

--- src/main/kotlin/fourthLesson/firstTask.kt
+++ src/main/kotlin/fourthLesson/firstTask.kt
@@ -1,2 +1,8 @@
 package fourthLesson
 
+fun main() {
+    val totalTables = 13
+    val bookedTablesToday = totalTables
+    val bookedTablesTomorrow = 9
+    println("Доступность столиков на сегодня: ${bookedTablesToday < totalTables}\nДоступность столиков на завтра: ${bookedTablesTomorrow < totalTables}")
+}
