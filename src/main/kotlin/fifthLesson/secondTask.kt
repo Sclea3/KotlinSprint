ee70fd5caa29c8f83e11d81acef94adcb9276c69
Sclea3 <sashamaty30@gmail.com>
2025-09-11 00:06:45 +0400

    KS-5-2 age check

--- src/main/kotlin/fifthLesson/secondTask.kt
+++ src/main/kotlin/fifthLesson/secondTask.kt
@@ -1,2 +1,14 @@
 package fifthLesson
 
+const val AGE_OF_MAJORITY = 18
+
+fun main() {
+    println("Введите год своего рождения:")
+    val userYearOfBirth = readln().toInt()
+    val currentUserAge = 2025 - userYearOfBirth
+
+    when  {
+        currentUserAge >= AGE_OF_MAJORITY -> println("Показать экран со скрытым контентом")
+        else -> println("Неподходящий возраст")
+    }
+}
