09e7777d312913159098b98c8c5ee75dea299401
Sclea3 <sashamaty30@gmail.com>
2025-09-11 12:17:13 +0400

    fixes

--- src/main/kotlin/fourthLesson/firstTask.kt
+++ src/main/kotlin/fourthLesson/firstTask.kt
@@ -1,8 +1,9 @@
 package fourthLesson
 
+const val TOTAL_TABLES = 13
+
 fun main() {
-    val totalTables = 13
     val bookedTablesToday = 13
     val bookedTablesTomorrow = 9
-    println("Доступность столиков на сегодня: ${bookedTablesToday < totalTables}\nДоступность столиков на завтра: ${bookedTablesTomorrow < totalTables}")
+    println("Доступность столиков на сегодня: ${bookedTablesToday < TOTAL_TABLES}\nДоступность столиков на завтра: ${bookedTablesTomorrow < TOTAL_TABLES}")
 }
