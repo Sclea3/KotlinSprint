4307957f56acc32badb9ffae035c09e1225e101f
Sclea3 <sashamaty30@gmail.com>
2025-09-11 16:33:10 +0400

    KS-5-5 new lottery

--- src/main/kotlin/fifthLesson/fifthTask.kt
+++ src/main/kotlin/fifthLesson/fifthTask.kt
@@ -2,8 +2,15 @@ package fifthLesson
 
 fun main() {
     val winNumbersList = List(3) {(0..42).random()}
-    print(winNumbersList)
     println("Введите числа для розыгрыша")
-    val userInputList = List(3) {readLine() ?: ""}
-    println(userInputList)
+    val userInputList = List(3) {readLine()?.toIntOrNull() ?:0}
+    val result = winNumbersList.intersect(userInputList)
+
+    when (result.size) {
+        0 -> println("Вы не угадали ни одного числа")
+        1 -> println("Выплата утешительного приза")
+        2 -> println("Вы угадали два числа и получаете крупный приз!")
+        else -> println("Вы угадали все числа и выиграли джекпот!")
+    }
+    println("Выиграшные числа: $winNumbersList")
 }
