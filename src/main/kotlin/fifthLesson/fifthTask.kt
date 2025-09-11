cdc7be680bb56fa2b1e0b39b83aeb8eca9739be8
Sclea3 <sashamaty30@gmail.com>
2025-09-11 16:18:30 +0400

    commit to save

--- src/main/kotlin/fifthLesson/fifthTask.kt
+++ src/main/kotlin/fifthLesson/fifthTask.kt
@@ -1,2 +1,9 @@
 package fifthLesson
 
+fun main() {
+    val winNumbersList = List(3) {(0..42).random()}
+    print(winNumbersList)
+    println("Введите числа для розыгрыша")
+    val userInputList = List(3) {readLine() ?: ""}
+    println(userInputList)
+}
