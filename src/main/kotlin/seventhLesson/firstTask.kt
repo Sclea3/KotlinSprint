246f1f32c495961bd37e040d5d3ec415bc6d00de
Sclea3 <sashamaty30@gmail.com>
2025-09-19 14:23:31 +0400

    KS 7-1 passw gen

--- src/main/kotlin/seventhLesson/firstTask.kt
+++ src/main/kotlin/seventhLesson/firstTask.kt
@@ -1,2 +1,12 @@
 package seventhLesson
 
+fun main() {
+    val counter = 6
+    var password = ""
+    var pool = ('a'..'z')+('1'..'9')
+    for (i in 1..counter) {
+        val charForPassw: Char = pool.random()
+        password += charForPassw
+    }
+    println(password)
+}
