49fe27c803bf2e700fc044550183746cb91abf71
Sclea3 <sashamaty30@gmail.com>
2025-09-19 12:14:53 +0400

    KS-6-2 timer

--- src/main/kotlin/sixthLesson/secondTask.kt
+++ src/main/kotlin/sixthLesson/secondTask.kt
@@ -1,2 +1,11 @@
 package sixthLesson
 
+fun main() {
+    println("Введите кол-во секунд:")
+    val neededSeconds = readln().toInt()
+
+    for (i in 1..neededSeconds) {
+        println("Секунда $i")
+        Thread.sleep(1000)
+    }
+}
