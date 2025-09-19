1de648053ebbd0d6ef4e4ddbf726f7a4075e2af4
Sclea3 <sashamaty30@gmail.com>
2025-09-19 16:45:34 +0400

    fix

--- src/main/kotlin/sixthLesson/thirdTask.kt
+++ src/main/kotlin/sixthLesson/thirdTask.kt
@@ -4,13 +4,9 @@ fun main() {
     println("Введите кол-во секунд:")
     val neededSeconds = readln().toInt()
 
-    var i = 0
-    var reverseCounter = 0
-    while (i < neededSeconds) {
-        reverseCounter = neededSeconds - i
-        println("Осталось секунд: $reverseCounter")
+    for (sec in neededSeconds downTo 1) {
+        println("Осталось секунд: $sec")
         Thread.sleep(1000)
-        i++
     }
     println("Время вышло")
 }
