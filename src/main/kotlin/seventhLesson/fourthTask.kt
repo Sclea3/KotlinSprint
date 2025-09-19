98df64fbc3d96a0c2e767f9873facd51e2bd6bfe
Sclea3 <sashamaty30@gmail.com>
2025-09-19 14:48:16 +0400

    KS-7-4 timer

--- src/main/kotlin/seventhLesson/fourthTask.kt
+++ src/main/kotlin/seventhLesson/fourthTask.kt
@@ -1,2 +1,12 @@
 package seventhLesson
 
+fun main() {
+    println("Введите кол-во секунд:")
+    val userInput = readln().toInt()
+
+    for (i in userInput downTo 0) {
+        Thread.sleep(1000)
+        println("Осталось секунд: $i")
+    }
+    println("Время вышло")
+}
