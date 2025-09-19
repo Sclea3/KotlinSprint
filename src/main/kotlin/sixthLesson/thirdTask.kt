852ae3ac19d32b91d97e8d1142609931c1bde1d6
Sclea3 <sashamaty30@gmail.com>
2025-09-19 12:45:30 +0400

    KS-6-3 new timer

--- src/main/kotlin/sixthLesson/thirdTask.kt
+++ src/main/kotlin/sixthLesson/thirdTask.kt
@@ -1,2 +1,16 @@
 package sixthLesson
 
+fun main() {
+    println("Введите кол-во секунд:")
+    val neededSeconds = readln().toInt()
+
+    var i = 0
+    var reverseCounter = 0
+    while (i < neededSeconds) {
+        reverseCounter = neededSeconds - i
+        println("Осталось секунд: $reverseCounter")
+        Thread.sleep(1000)
+        i++
+    }
+    println("Время вышло")
+}
