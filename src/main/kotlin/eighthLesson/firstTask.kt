294356e18cfa14b7b3fa4f2b477a3375cfaddae1
Sclea3 <sashamaty30@gmail.com>
2025-09-19 15:13:39 +0400

    KS-8-1 views count

--- src/main/kotlin/eighthLesson/firstTask.kt
+++ src/main/kotlin/eighthLesson/firstTask.kt
@@ -1,2 +1,25 @@
 package eighthLesson
 
+import javax.swing.text.View
+
+fun main() {
+    val mondayViews = 123
+    val tuesdayViews = 321
+    val wednesdayViews = 1221
+    val thursdayViews = 55
+    val fridayViews = 53
+    val saturdayViews = 312
+    val sundayViews = 42
+
+    val arrayOfViews = intArrayOf(
+        mondayViews,
+        tuesdayViews,
+        wednesdayViews,
+        thursdayViews,
+        fridayViews,
+        saturdayViews,
+        sundayViews
+    )
+    val totalViews = arrayOfViews.sum()
+    println("Всего просмотров за неделю: $totalViews")
+}
