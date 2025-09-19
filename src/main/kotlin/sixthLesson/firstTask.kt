09f4642abefbec81bad5d9bd7f496bd7e98fc52d
Sclea3 <sashamaty30@gmail.com>
2025-09-19 12:08:57 +0400

    KS-6-1 Login

--- src/main/kotlin/sixthLesson/firstTask.kt
+++ src/main/kotlin/sixthLesson/firstTask.kt
@@ -1,2 +1,23 @@
 package sixthLesson
 
+fun main() {
+    println("Введите желаемый логин:")
+    val userLogin = readln()
+    println("Введите пароль:")
+    val userPass = readln()
+    println("Регистрация завершена")
+
+    while (true) {
+        println("Введите логин:")
+        val inputLogin = readln()
+        println("Введите пароль:")
+        val inputPassword = readln()
+
+        if (inputLogin == userLogin && inputPassword == userPass) {
+            println("Авторизация прошла успешно")
+            break
+        } else {
+            println("Неверный логин или пароль. Попробуйте снова")
+        }
+    }
+}
