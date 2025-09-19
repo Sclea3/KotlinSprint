ca5985c891989b248ef381c3ac77ec9ba122321b
Sclea3 <sashamaty30@gmail.com>
2025-09-19 14:24:11 +0400

    fix var to val

--- src/main/kotlin/seventhLesson/firstTask.kt
+++ src/main/kotlin/seventhLesson/firstTask.kt
@@ -3,7 +3,7 @@ package seventhLesson
 fun main() {
     val counter = 6
     var password = ""
-    var pool = ('a'..'z')+('1'..'9')
+    val pool = ('a'..'z')+('1'..'9')
     for (i in 1..counter) {
         val charForPassw: Char = pool.random()
         password += charForPassw
