074e2356185b2783c0b2a7f3c87c84d0671ece16
Sclea3 <sashamaty30@gmail.com>
2025-09-20 21:00:14 +0400

    fix

--- src/main/kotlin/seventhLesson/fifthTask.kt
+++ src/main/kotlin/seventhLesson/fifthTask.kt
@@ -1,8 +1,11 @@
 package seventhLesson
 
 fun main() {
-    println("Введите длину пароля:")
+    println("Введите длину пароля (не менее 6):")
     val userInput = readln().toInt()
+    if (userInput < 6) {
+        println("Длина пароля не может быть меньше 6 символов")
+    }
     val pool = ('a'..'z') + ('A'..'Z') + ('0'..'9')
     var passw = ""
 
