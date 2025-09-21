8800574e938ac094624a8160784b5d5f805f3dfa
Sclea3 <sashamaty30@gmail.com>
2025-09-21 18:15:25 +0400

    KS-8-5 ingredients array

--- src/main/kotlin/eighthLesson/fifthTask.kt
+++ src/main/kotlin/eighthLesson/fifthTask.kt
@@ -1,2 +1,16 @@
 package eighthLesson
 
+fun main() {
+    println("Сколько ингридиентов будете вводить?")
+    val userDigitsCountInput = readln().toInt()
+
+    if (userDigitsCountInput <= 0) {
+        println("Некорректное количество чисел.")
+        return
+    }
+
+    println("Введите по очереди свои числа:")
+    val userDigitsArray = Array(userDigitsCountInput) { readln().toInt() }
+
+    println("Введенный массив чисел:\n${userDigitsArray.joinToString(", ")}")
+}
