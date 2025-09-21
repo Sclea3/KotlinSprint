f12cf873fb2d88278b4f019999c807ef4351922c
Sclea3 <sashamaty30@gmail.com>
2025-09-21 18:00:06 +0400

    fixes

--- src/main/kotlin/eighthLesson/fourthTask.kt
+++ src/main/kotlin/eighthLesson/fourthTask.kt
@@ -3,15 +3,21 @@ package eighthLesson
 fun main() {
     val arrayOfIngredientsForSmth = arrayOf("морковь", "капуста", "кабачок", "рыба")
     println("Список ингредиентов:")
-    for (i in arrayOfIngredientsForSmth.indices) {
-        println(arrayOfIngredientsForSmth[i])
-    }
+    arrayOfIngredientsForSmth.forEach { println(it) }
+
     println("Введите ингредиент который хотите заменить:")
     val userInputWhatToReplace = readln().lowercase()
+    if (userInputWhatToReplace !in arrayOfIngredientsForSmth) {
+        println("Такого ингридиента нет в списке")
+        return
+    }
+
     println("Введите на какой ингредиент хотите заменить:")
     val userInputWhatToReplaceWith = readln().lowercase()
 
     val i = arrayOfIngredientsForSmth.indexOf(userInputWhatToReplace)
 
     arrayOfIngredientsForSmth[i] = userInputWhatToReplaceWith
+
+    println("Готово! Вы сохранили следующий список: ${arrayOfIngredientsForSmth.forEach { println(it) }}")
 }
