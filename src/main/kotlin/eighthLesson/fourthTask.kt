83d8de9c2690bcb6974cc9fd305cce0316a2a43f
Sclea3 <sashamaty30@gmail.com>
2025-09-21 17:56:08 +0400

    KS-8-4 ingridient replacer

--- src/main/kotlin/eighthLesson/fourthTask.kt
+++ src/main/kotlin/eighthLesson/fourthTask.kt
@@ -1,2 +1,17 @@
 package eighthLesson
 
+fun main() {
+    val arrayOfIngredientsForSmth = arrayOf("морковь", "капуста", "кабачок", "рыба")
+    println("Список ингредиентов:")
+    for (i in arrayOfIngredientsForSmth.indices) {
+        println(arrayOfIngredientsForSmth[i])
+    }
+    println("Введите ингредиент который хотите заменить:")
+    val userInputWhatToReplace = readln().lowercase()
+    println("Введите на какой ингредиент хотите заменить:")
+    val userInputWhatToReplaceWith = readln().lowercase()
+
+    val i = arrayOfIngredientsForSmth.indexOf(userInputWhatToReplace)
+
+    arrayOfIngredientsForSmth[i] = userInputWhatToReplaceWith
+}
