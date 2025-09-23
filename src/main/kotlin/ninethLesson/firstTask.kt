ac85edf20c995cf5218df3321bf113ff58fde472
Sclea3 <sashamaty30@gmail.com>
2025-09-23 14:20:37 +0400

    KS-9-1 list of ingredients

--- src/main/kotlin/ninethLesson/firstTask.kt
+++ src/main/kotlin/ninethLesson/firstTask.kt
@@ -1,2 +1,7 @@
 package ninethLesson
 
+fun main() {
+    val ingredientsNames = listOf("морковь", "капуста", "помидор")
+    println("В рецепте есть следующие ингредиенты:\n${ingredientsNames.joinToString("\n")}")
+//    Либо ingredientsNames.forEach {println(it)}
+}
