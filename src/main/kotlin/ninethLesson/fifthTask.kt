300441df89747c5815c08a582a038727a7163123
Sclea3 <sashamaty30@gmail.com>
2025-09-23 15:00:02 +0400

    KS-9-5 Unique user ingredients input

--- src/main/kotlin/ninethLesson/fifthTask.kt
+++ src/main/kotlin/ninethLesson/fifthTask.kt
@@ -1,2 +1,24 @@
 package ninethLesson
 
+fun main() {
+    val ingredients = mutableSetOf<String>()
+
+    for (i in 1..5) {
+        println("Введите ингредиент №$i:")
+        val input = readln().trim()
+        ingredients.add(input)
+    }
+
+    val listOfIngredients = ingredients.toList().sorted()
+
+    val resultList = mutableListOf<String>()
+    for ((index, item) in listOfIngredients.withIndex()) {
+        if (index == 0) {
+            resultList.add(item.replaceFirstChar { it.uppercase() })
+        } else {
+            resultList.add(item)
+        }
+    }
+
+    println(resultList.joinToString(", "))
+}
