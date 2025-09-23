a6138966b3fe50eaac27373bdb49dc9229d17fed
Sclea3 <sashamaty30@gmail.com>
2025-09-23 14:37:17 +0400

    KS-9-3 calculate ingredients

--- src/main/kotlin/ninethLesson/thirdTask.kt
+++ src/main/kotlin/ninethLesson/thirdTask.kt
@@ -1,2 +1,11 @@
 package ninethLesson
 
+fun main() {
+    val ingredientsForOmelet = listOf(2, 50, 15)
+    val userIngredientsForOmelet = mutableListOf<Int>()
+    println("Введите кол-во порций:")
+    val userInput = readln().toInt()
+    ingredientsForOmelet.forEach { userIngredientsForOmelet.add(it * userInput) }
+
+    println("На $userInput порций вам понадобится: яиц - ${userIngredientsForOmelet[0]} шт, молока - ${userIngredientsForOmelet[1]} мл, сливочного масла - ${userIngredientsForOmelet[2]} гр")
+}
