25a906d518e2d624fe0d78ecbc7bc89da6c7b8aa
Sclea3 <sashamaty30@gmail.com>
2025-09-23 14:29:10 +0400

    KS-9-2 add to list of ingredients

--- src/main/kotlin/ninethLesson/secondTask.kt
+++ src/main/kotlin/ninethLesson/secondTask.kt
@@ -1,2 +1,18 @@
 package ninethLesson
 
+const val DISAGREE_NO = "no"
+const val DISAGREE_N = "n"
+
+fun main() {
+    val listOfIngredients = mutableListOf("морковь", "капуста", "кабачок")
+    println("В рецепте есть базовые ингредиенты: ${listOfIngredients.joinToString(", ") }")
+    println("Желаете добавить еще? Y/N")
+    val userInputAgreeToAdd = readln()
+    if (userInputAgreeToAdd.equals(DISAGREE_NO) || userInputAgreeToAdd.equals(DISAGREE_N)) {
+        return
+    }
+    println("Какой ингредиент вы хотите добавить?")
+    val userInputToAdd = readln()
+    listOfIngredients.add(userInputToAdd)
+    println("Теперь в рецепте есть следующие ингредиенты: ${listOfIngredients.joinToString(", ") }")
+}
