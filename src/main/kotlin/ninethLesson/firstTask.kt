649edab487b533eddada5bb4720cd453a92f3cbd
Sclea3 <sashamaty30@gmail.com>
2025-09-25 22:54:06 +0400

    fix

--- src/main/kotlin/ninethLesson/firstTask.kt
+++ src/main/kotlin/ninethLesson/firstTask.kt
@@ -3,5 +3,4 @@ package ninethLesson
 fun main() {
     val ingredientsNames = listOf("морковь", "капуста", "помидор")
     println("В рецепте есть следующие ингредиенты:\n${ingredientsNames.joinToString("\n")}")
-//    Либо ingredientsNames.forEach {println(it)}
 }
